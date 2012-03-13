ff17f2cc87b600bfde67c4d6aa40f792d2d097f6
Graham Conzett <conzett@gmail.com>
2012-03-13 12:39:48 -0400

    Update example test file
    Add module with setup and teardown, move fixture wrapper creation to
    setup.

--- test/tests.js
+++ test/tests.js
@@ -1,7 +1,13 @@
-test("Sample test", function () {
+module("foo", {
+    setup: function() {
+    	// add dummy element to fixture
+    	$('#qunit-fixture').append('<div id="target"></div>');
+    },
+    tearDown: function() {
 
-    $('#qunit-fixture').append('<div id="target"></div>');
-    $('#target').scheduler();
+    });
 
-    equal("test value", "expected value", "Expect the result to be...");
-});
+	test("Sample test", function () {
+    	equal("test value", "expected value", "Expect the result to be...");
+	});
+}
